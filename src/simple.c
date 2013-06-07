6d522383ae96046f74c7b74611fe07254584666c
brandonelman <tuc42002@temple.edu>
2013-06-07 13:35:13 -0400

    Finalized simple.c up till start acquisition step. No calibration methods.

--- src/simple.c
+++ src/simple.c
@@ -1,3 +1,38 @@
+#define V1729_RESET_BOARD 			0x0800
+#define V1729_START_ACQUISITION		0x1700
+#define V1729_TRIGGER_TYPE 			0x1D00
+#define V1729_FP_FREQUENCY 			0x8100
+#define V1729_INTERRUPT				0x8000
+#define V1729_LOAD_TRIGGER_THS		0x0900
+#define V1729_THRESHOLD      		0x0A00
+#define V1729_SOFTWARE_TRIGGER		0x1C00
+#define V1729_RAM_DATA_VME			0x0D00
+#define V1729_RAM_INT_ADD_LSB		0x0E00
+#define V1729_RAM_INT_ADD_MSB		0x0F00
+#define V1729_PRETRIG_LSB			0x1800
+#define V1729_PRETRIG_MSB			0x1900
+#define V1729_POSTTRIG_LSB			0x1A00
+#define V1729_POSTTRIG_MSB			0x1B00
+#define V1729_TRIGGER_CHANNEL_SRC	0x1E00
+#define V1729_TRIG_REC				0x2000
+#define V1729_FAST_READ_MODES		0x2100
+#define V1729_NB_OF_COLS_TO_READ	0x2200
+#define V1729_CHANNEL_MASK			0x2300
+#define V1729_FPGA_VERSION			0x8200
+#define V1729_EN_VME_IRQ			0x8300
+#define V1729_EN_SYNC_OUT			0x1100
+#define V1729_MODE_REGISTER         0x0300
+
+#define V1729                       0x3
+#define V1729A                      0xF
+
+
+
+#define CAENBASEADDRESS  0x30010000
+#define V1729_RAM_DEPH 10252
+#define V1729_NOS 2560
+#define V1729_VERNIER_DEPH 65536
+
 
 #include <stdlib.h>
 #include <math.h>
@@ -56,6 +91,10 @@ int main(int argc, void *argv[])
   uint32_t vme_data; //Data holder for writing and reading
   
   uint32_t trig_lev = 0x6ff; 
+  uint32_t active_channel;
+  uint32_t trig_type;
+  uint32_t num_columns;
+  uint32_t post_trig;
 
   /* Parameters */
   unsigned short addr_mode = cvA32_U_DATA;
@@ -63,7 +102,7 @@ int main(int argc, void *argv[])
   CVDataWidth data_size = cvD32;  
 
   /* Create opaque handle for interacting with VX2718 Board */
-  if ( CAENVME_Init(vme_board, linkm device, &handle) != cvSuccess )
+  if ( CAENVME_Init(vme_board, link, device, &handle) != cvSuccess )
   {
     printf("\n\n Error opening VX2718\n");
     exit(1);
@@ -111,16 +150,125 @@ int main(int argc, void *argv[])
 
   else printf("Load Trigger threshold command successful");
 
-  printf("Attempting to load pilot frequency"); 
+/*  printf("Attempting to load pilot frequency"); 
   vme_addr = CAENBASEADDRESS + V1729_FP_FREQUENCY;
 
   ret = CAENVME_ReadCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
 
   if (ret != cvSuccess)
   {
-    printf("Loading pilot frequency failed with error: %d", ret);
+    printf("Loading pilot freq uency failed with error: %d", ret);
   }  
 
   else printf("Loading pilot frequency successful");
+  UNNECESSARY: Pilot Frequency defaults to 2Gs/s */
 
+  //Initializing rest of parameters....
+
+  printf("Attempting to determine number of columns to read from MATACQ ");
+  vme_addr = CAENBASEADDRESS + V1729_NB_OF_COLS_TO_READ;
+  ret = CAENVME_ReadCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+  if (ret != cvSuccess)
+  {
+    printf("Loading num. of columns failed with error: %d \n", ret);
+  }  
+
+  else printf("Load number of columns successful\n");
+
+  num_columns = vme_data & 0xff; /*0xff is 11111111 in binary. 
+                                 Default value is 128 so
+                                 128&0xff = 128, all the columns */
+
+  printf("Attempting to determine trigger type...\n");
+  vme_addr = CAENBASEADDRESS + V1729_TRIGGER_TYPE;
+  ret = CAENVME_ReadCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+  if (ret != cvSuccess)
+  {
+    printf("Loading trigger type failed with error: %d \n", ret);
+  }  
+
+  else printf("Load Trigger Type successful\n");
+
+  trig_type = vme_data & 0x3f; 
+
+  printf("Attempting to determine active channel...\n");
+  vme_addr = CAENBASEADDRESS + V1729_CHANNEL_MASK;
+  ret = CAENVME_ReadCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+  if (ret != cvSuccess)
+  {
+    printf("Loading active channel failed with error: %d \n", ret);
+  }  
+
+  else printf("Load active channel successful\n");
+
+  active_channel = vme_data & 0x3f; 
+
+  /*Determining the POSTTRIG requires reading two registers:
+    POSTTRIG_MSB and POSTTRIG_LSB. The value must be read for
+    use in data reordering later. */
+  printf("Attempting to determine POSTTRIG...\n");
+  vme_addr = CAENBASEADDRESS + V1729_POSTTRIG_LSB;
+  ret = CAENVME_ReadCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+
+  if (ret != cvSuccess)
+  {
+    printf("Loading POSTTRIG_LSB failed with error: %d \n", ret);
+  }  
+
+  else printf("Load POSTTRIG_LSB successful\n");
+
+  post_trig = vme_data & 0x3f; 
+
+  vme_addr = CAENBASEADDRESS + V1729_POSTTRIG_MSB;
+  ret = CAENVME_ReadCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+
+  if (ret != cvSuccess)
+  {
+    printf("Loading POSTTRIG_MSB failed with error: %d \n", ret);
+  }  
+
+  else printf("Load POSTTRIG_MSB successful\n");
+
+  post_trig = post_trig + (vme_data & 0xFF)*256; 
+
+  //Send start acquisition signal  
+  vme_addr = CAENBASEADDRESS + V1729_START_ACQUISITION;
+  vme_data = 1;
+  ret = CAENVME_WriteCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+ 
+  //Send Software Trigger after waiting PRETRIG <- Not sure how to wait
+  printf("Sending Software Trigger...\n"); 
+  vme_data = 1;
+  vme_addr = CAENBASEADDRESS + V1729_SOFTWARE_TRIGGER;
+
+  ret = CAENVME_WriteCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+
+  if (ret != cvSuccess)
+  {
+    printf("Sending Software Trigger failed with error %d", ret);
+  }  
+
+  else printf("Sending Software Trigger succesful");
+
+  //Send Reset to End Acquisition
+  printf("Attempting to reset board.");
+  vme_addr = CAENBASEADDRESS + V1729_RESET_BOARD;
+  vme_data = 1;
+  ret = CAENVME_WriteCycle(handle, vme_addr, &vme_data, addr_mode, data_size); 
+  if (ret != cvSuccess)
+  {
+    printf("Reset failed with error %d", ret);
+  }  
+
+  else printf("Reset succesfully");
+
+  ret = CAENVME_End(handle);  
+  if (ret != cvSuccess)
+  {
+    printf("Closing board failed with error %d", ret);
+  }  
+
+  else printf("Closed Board succesfully");
+
+ 
 }
