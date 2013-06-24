4ec65c009a7383a4a74cdb45a653f501703de167
brandonelman <tuc42002@temple.edu>
2013-06-24 12:02:50 -0400

    Added source code and header for manipulating the V812 CFD

---
+++ include/V812.h
@@ -0,0 +1,52 @@
+
+
+/*Register Addresses */
+#define V812_THRESHOLD_CH0  0x00
+#define V812_THRESHOLD_CH1  0x02
+#define V812_THRESHOLD_CH2  0x04
+#define V812_THRESHOLD_CH3  0x06
+#define V812_THRESHOLD_CH4  0x08
+#define V812_THRESHOLD_CH5  0x0A
+#define V812_THRESHOLD_CH6  0x0C
+#define V812_THRESHOLD_CH7  0x0E
+#define V812_THRESHOLD_CH8  0x10
+#define V812_THRESHOLD_CH9  0x12
+#define V812_THRESHOLD_CH10 0x14
+#define V812_THRESHOLD_CH11 0x16
+#define V812_THRESHOLD_CH12 0x18
+#define V812_THRESHOLD_CH13 0x1A
+#define V812_THRESHOLD_CH14 0x1C
+#define V812_THRESHOLD_CH15 0x1E
+#define V812_OUTPUT_WIDTH_1 0x40
+#define V812_OUTPUT_WIDTH_2 0x42
+#define V812_DEAD_TIME_1 0x44
+#define V812_DEAD_TIME_2 0x46
+#define V812_MAJORITY_THRESHOLD 0x48
+#define V812_PATTERN_INHIBIT 0x4A
+#define V812_TEST_PULSE 0x4C
+#define V812_FIXED_CODE 0xFA
+#define V812_MODULE_TYPE 0xFC
+#define V812_VERSION_AND_SERIAL 0xFE
+
+#define V812_BASE_ADDRESS 0xEE000000 
+
+#include "CAENVMElib.h"
+/***************************************************
+                Function Declarations
+***************************************************/
+
+CVErrorCodes write_to_v812(uint32_t, uint32_t);
+CVErrorCodes set_channel_threshold(int ch_num, int threshold);
+
+/* CH_SECTION means 1 or 2. Choose 1 for channels 0-7, and 2 for 8-15 */
+/* See manual for count value correspondence to time */
+CVErrorCodes set_output_width(int ch_section, int set_count);
+CVErrorCodes set_dead_time(int ch_section, int set_count);
+/* NOTE: Actual Dead Time is the greater of the set output width and
+   dead time values */
+
+/* MAJTHR = NINT[(MAJLEV*50 - 25)/4] where NINT is NEAREST INTEGER */
+CVErrorCodes set_majority_threshold(int maj_lev);
+CVErrorCodes send_test_pulse(void);
+
+
---
+++ src/V812.c
@@ -0,0 +1,112 @@
+
+
+#include "CAENVMElib.h"
+#include "V1729.h"
+#include "V812.h"
+
+CVErrorCodes write_to_v812(uint32_t vme_addr, uint32_t data)
+{
+  CVDataWidth data_size = cvD16;
+  unsigned short addr_mode = cvA32_U_DATA;
+  vme_addr = vme_addr + V812_BASE_ADDRESS;
+
+  return CAENVME_WriteCycle(handle, vme_addr, &data, addr_mode, data_size);
+}  
+
+CVErrorCodes set_channel_threshold(int ch_num, int threshold)
+{
+uint32_t vme_addr; 
+
+switch(ch_num)
+  {
+    case 0: vme_addr = V812_THRESHOLD_CH0; 
+            break;
+    case 1: vme_addr = V812_THRESHOLD_CH1; 
+            break;
+    case 2: vme_addr = V812_THRESHOLD_CH2; 
+            break;
+    case 3: vme_addr = V812_THRESHOLD_CH3; 
+            break;
+    case 4: vme_addr = V812_THRESHOLD_CH4; 
+            break;
+    case 5: vme_addr = V812_THRESHOLD_CH5; 
+            break;
+    case 6: vme_addr = V812_THRESHOLD_CH6; 
+            break;
+    case 7: vme_addr = V812_THRESHOLD_CH7; 
+            break;
+    case 8: vme_addr = V812_THRESHOLD_CH8; 
+            break;
+    case 9: vme_addr = V812_THRESHOLD_CH9; 
+            break;
+    case 10: vme_addr = V812_THRESHOLD_CH10; 
+             break;
+    case 11: vme_addr = V812_THRESHOLD_CH11;
+             break;
+    case 12: vme_addr = V812_THRESHOLD_CH12;
+             break;
+    case 13: vme_addr = V812_THRESHOLD_CH13;
+             break;
+    case 14: vme_addr = V812_THRESHOLD_CH14;
+             break;
+    case 15: vme_addr = V812_THRESHOLD_CH15;
+             break;
+
+    default: printf("Bad choice of channel!");
+             return cvInvalidParam; 
+  }         
+
+  return write_to_v812(vme_addr, threshold);
+}
+
+CVErrorCodes set_output_width(int ch_section, int set_count)
+{
+  uint32_t vme_addr; 
+
+  if (ch_section == 1)
+    vme_addr = V812_OUTPUT_WIDTH_1;  
+
+  else if(ch_section == 2)
+    vme_addr = V812_OUTPUT_WIDTH_2;
+  
+  else
+  {
+    printf("Invalid channel section! Choose either 1 or 2!");
+    return cvInvalidParam;
+  }
+  
+  return write_to_v812(vme_addr, set_count);
+}
+
+CVErrorCodes set_dead_time(int ch_section, int set_count)
+{
+  uint32_t vme_addr; 
+
+  if (ch_section == 1)
+    vme_addr = V812_DEAD_TIME_1; 
+
+  else if(ch_section == 2)
+    vme_addr = V812_DEAD_TIME_2;
+  
+  else
+  {
+    printf("Invalid channel section! Choose either 1 or 2!");
+    return cvInvalidParam;
+  }
+  
+  return write_to_v812(vme_addr, set_count);
+}
+
+CVErrorCodes set_majority_threshold(int maj_lev) 
+{
+  uint32_t vme_addr = V812_MAJORITY_TRESHOLD; 
+  int majority_treshold = (int)((majlev*50.0-25.0)/4.0);
+
+  return write_to_v812(vme_addr, majority_treshold);
+}
+
+CVErrorCodes send_test_pulse(void)
+{
+  return write_to_v812(V812_TEST_PULSE, 1);
+}
+
